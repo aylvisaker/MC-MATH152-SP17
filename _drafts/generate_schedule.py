b0d0589b9bb0f682baeed9f3b59b5eb37915c38e
Andrew Ylvisaker <aylvisaker@protonmail.com>
2017-02-15 11:16:34 -0600

    fixed schedule

--- _drafts/generate_schedule.py
+++ _drafts/generate_schedule.py
@@ -12,15 +12,15 @@ discussions = [item[1] for item in source]
 schedule_markup.write('---\nlayout: post\ntitle: "Course Schedule"\n')
 schedule_markup.write('categories: [syllabus]\ntags: [syllabus]\n')
 schedule_markup.write('description: MC-MATH-152\n---\n\n')
-schedule_markup.write('|Week|Start Date|Monday|Tuesday|Wednesday|Friday|\n')
-schedule_markup.write('|:---:|:---:|:---: |:---:  |:---:    |:---: |\n')
+schedule_markup.write('|Week|Start Date|Monday|Tuesday|Wednesday|Thursday|Friday|\n')
+schedule_markup.write('|:---:|:---:|:---:|:---:|:---:|:---:|:---: |\n')
 iterator = 0
 lab_iterator = 0
 for week in range(len(academic_calendar)):
     row = '|' + str(week + 1) + '|' + str(start + datetime.timedelta(days = 7 * week))[-5:] + '|'
     if academic_calendar[week][3]:
         lab_iterator += 1
-    for day in [0,1,2,4]:
+    for day in [0,1,2,3,4]:
         current_day = start + datetime.timedelta(days = 7 * week + day)
         if not academic_calendar[week][day]:
             row += 'NO CLASS' + '|'
